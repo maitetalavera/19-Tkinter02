492671829c1e16319283685fac733976f1e6ea9a
maitetalavera <mtalavera001@student.franciscan.edu>
2024-03-28 20:11:06 -0400

    m2

--- src/m2_frames.py
+++ src/m2_frames.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # In this module, all of the _todo_ items will be in one comment because you
 # will be modifying the same block of code as you go.
 #
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #
 #   First, create a tkinter window called window.
 #
@@ -15,7 +15,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (2 pts)
+# DONE: 2. (2 pts)
 #
 #   Now, create a frame called frm_a and add a label called lbl_a to it that
 #   contains the text "Frame A".
@@ -25,7 +25,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 3. (2 pts)
+# DONE: 3. (2 pts)
 #
 #   Now, create a frame called frm_b and add a label called lbl_b to it that
 #   contains the text "Frame B".
@@ -35,7 +35,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 4. (1 pt)
+# DONE: 4. (1 pt)
 #
 #   Now, edit your frames to have different background colors. You can choose
 #   the colors, but they must both be different.
@@ -43,10 +43,35 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 5. (1 pt)
+# DONE: 5. (1 pt)
 #
 #   Now, edit frm_b to have one of the reliefs that we looked at in our code
 #   along.
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+
+import tkinter as tk
+
+window = tk.Tk()
+
+frm_a=tk.Frame(master=window, relief=tk.GROOVE, borderwidth=5)
+frm_b=tk.Frame()
+
+lbl_a=tk.Label(master=frm_a, text="Frame A", bg="red")
+lbl_a.pack()
+lbl_b=tk.Label(master=frm_b, text="Frame B", bg="purple")
+lbl_b.pack()
+
+frm_a.pack()
+frm_b.pack()
+
+border_effects={
+    "flat":tk.FLAT, 
+    "sunken":tk.SUNKEN, 
+    "raised":tk.RAISED,
+    "groove":tk.GROOVE,
+    "ridge":tk.RIDGE
+}
+
+window.mainloop()
